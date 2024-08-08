8464e98b1e8a98c7918cc73c9417267b8bd7440a
SamsonOyede <oyedetobi49@gmail.com>
2024-08-08 20:43:17 +0100

    add EC2 instance Id

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ ec2 = boto3.client(AWS_SERVICE)
 
 def StartInstance():
     try:
-        response = ec2.start_instances(InstanceIds=[''], DryRun=False);
+        response = ec2.start_instances(InstanceIds=['i-020aaf861101680e9'], DryRun=False);
         currentState = response['StartingInstances'][0]['CurrentState']['Name']
         if(currentState == Pending or currentState == Running):
             print('EC2 instance is staring up!!!!')
